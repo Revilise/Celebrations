76948a94f665c70c1bc7a8d45b76984411f781b4
= <nice.mutnykh@mail.ru>
2021-03-21 01:04:47 +0300

    unselected item

--- Home.cs
+++ Home.cs
@@ -17,6 +17,7 @@ namespace Celebrations
         public Home()
         {
             InitializeComponent();
+            comboBox1.SelectedItem = comboBox1.Items[0];
         }
 
         private void opener_Click(object sender, EventArgs e)
